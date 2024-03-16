adfbd9767f1f526ff6eabd2eb982f6093fcff708
Aleks270875 <162806747+Aleks270875@users.noreply.github.com>
2024-03-16 06:31:12 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,6 @@
+immutable_var = 7, 8, 'Tambov', True
+print(immutable_var)
+mutable_list = [7, 8, 'Tambov', True]
+mutable_list[0] = 1
+mutable_list.append('kedi')
+print(mutable_list)
---
+++ homework5.py
@@ -0,0 +1,15 @@
+my_list = ['lemon', 'kiwi', 'plum', 'fig', 'banana', 'mango']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] =  'apple'
+print(my_list)
+my_dict = {}
+my_dict ['table'] = 'стол'
+my_dict ['map'] = 'карта'
+my_dict ['pen'] = 'ручка'
+my_dict ['sun'] = 'солнце'
+print(my_dict)
+print(my_dict['sun'])
+my_dict ['moon'] = 'луна'
+print(my_dict)
